d32d8c558a4b0e86fb83d3a41daa3d7f537f457c
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-05-08 18:39:12 -0600

    lesson3

---
+++ lesson3/js/index.js
@@ -0,0 +1,4 @@
+//document.querySelector('#theyear').textContent = document.getFullYear;
+//document.getElementById("theyear").innerHTML = new Date().getFullYear();
+document.querySelector('#theyear').textContent = new Date().getFullYear();
+document.querySelector('#lastmod').textContent = document.lastModified;
