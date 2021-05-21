8282d0ebd1a9075da4d5d1c3b4200b434c23a834
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-05-20 21:53:15 -0600

    javascript

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -9,3 +9,7 @@ document.querySelector('#theyear').textContent = new Date().getFullYear();
 document.querySelector('#lastmod').textContent = document.lastModified;
 var d = new Date();
 document.getElementById("date").innerHTML = d;
+
+if (d.getDay() == 5) {
+    document.querySelector(".pancake").style.display = "block";
+}
