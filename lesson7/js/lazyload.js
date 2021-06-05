53ef5515b0b26141b789e3dac2649b3efc63b584
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-06-05 10:05:07 -0600

    update js

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,37 +1,37 @@
-/*const images = document.querySelectorAll ("[data-src]");
+//Get all images on the page with data-src
+const imagesToLoad = document.querySelectorAll ("img[data-src]");
 
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!srcc) {
-        return;
-    }
-    img.src = src;
-}
+const loadImages = (image) => {
+    Image.setAttribute("src", Image.getAttribute("data-src"));
+    Image.onload = () => {
+        Image.removeAttribute("data-src");
+    };
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: "0px 0px -580px 0px"
+    rootMargin: "0px 0px 50px 0px"
 };
 
-const imgObserver = new IntersectionObserver((entries,  
-imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+    });
+    }, imgOptions);
 
-}, imgOptions);
+    imagesToLoad.forEach(img => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
 
-images.forEach(image => {
-    imgObserver.observe(image);
-}); */
-
-const imagesToLoad = document.querySelectorAll("img{data-src]");
+/*const imagesToLoad = document.querySelectorAll("img{data-src]");
 
 const loadImages = (Image) => {
     Image.setAttribute("src", Image.getAttribute("data-src"));
@@ -41,7 +41,7 @@ const loadImages = (Image) => {
 };
  const imgOptions = {
     threshold: 1, 
-    rootMargin: "0px 0px 40px 0px"
+    rootMargin: "0px 0px -250px 0px"
  };
 
  if ("IntersectionObserver" in window) {
@@ -61,6 +61,4 @@ const loadImages = (Image) => {
      imagesToLoad.forEach((img)=>{
          loadImages(img);
      });
- }
-
-
+ } */
