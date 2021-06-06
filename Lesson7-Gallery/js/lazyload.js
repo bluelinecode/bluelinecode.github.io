c380ac818784a7a02814e1e9b6cdb622bc9ec644
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-06-05 23:12:38 -0600

    updates

---
+++ Lesson7-Gallery/js/lazyload.js
@@ -0,0 +1,36 @@
+//Get all images on the page with data-src
+const imagesToLoad = document.querySelectorAll ("img[data-src]");
+
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver(items => {
+        items.forEach(item => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+    });
+    }, imgOptions);
+
+    imagesToLoad.forEach(img => {
+        imgObserver.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach(img => {
+        loadImages(img);
+    });
+}
+
