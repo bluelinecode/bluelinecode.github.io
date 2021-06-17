53f50044735d9ac19ca365b7f5164ad64fbbedd4
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-06-17 11:09:15 -0600

    rough overview

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -8,19 +8,33 @@ fetch(requestURL)
     //console.table(jsonObject);  // temporary checking for valid response and data parsing
     const prophets = jsonObject['prophets'];
 
-    for (let i = 0; i < prophets.length; i++ ) {
+    prophets.forEach (prophet => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
+        
+        let birthplace = document.createElement('birthplace');
+        let dob= document.createElement('birthdate');
         let img = document.createElement('img');
 
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        img.setAttribute('src', prophets[i].imageurl);
-        img.setAttribute('alt', `The official portrait of ${prophets[i].name} ${prophets[i].lastname}!`);
+        birthplace.innerHTML = `${prophet.birthplace}`;
+        dob.innerHTML = `${prophet.birthdate}`;
+
+        h2.innerHTML = `${prophet.name} <span> ${prophet.lastname}</span>`;
+        img.setAttribute('src', prophet.imageurl);
+        img.setAttribute('alt', `The official portrait of ${prophet.name} ${prophet.lastname}!`);
 
         card.appendChild(h2);
+        card.append(birthplace);
+        card.append(dob);
+        card.append(img);
+
+        document.querySelector('div.cards').append(card);
+    })
+    
+
 
         document.querySelector('div.cards').appendChild(card);
     }
 
-  });
+  );
 
