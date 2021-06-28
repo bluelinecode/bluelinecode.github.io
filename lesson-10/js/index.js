a9330c98837dc4f51c85f9d28bd2f856a9ebef66
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-06-28 14:26:12 -0600

    a lot of updates

---
+++ lesson-10/js/index.js
@@ -0,0 +1,54 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')}; 
+
+document.querySelector('#theyear').textContent = new Date().getFullYear();
+document.querySelector('#lastmod').textContent = document.lastModified;
+var d = new Date();
+document.getElementById("date").innerHTML = d;
+
+if (d.getDay() == 6) {
+    document.querySelector(".pancake").style.display = "block";
+}
+
+
+//Weather box
+
+const cityid = "5604473";
+const APPID = "cdc0b1e423f5c1ece1c635b594e5fc76";
+
+
+const apiURL = `http://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const temperature = document.querySelector('#temperature');
+    temperature.textContent = jsObject.main.temp;  
+    // const currently = document.querySelector('#currently');
+    // currently.textContent = jsObject.weather.description;
+    const desc = jsObject.weather[0].description; //considering this instead of 'currently'
+    desc.innerHTML = `<strong>${desc.toUPPERCase}</strong>`;
+    const windspeed = document.querySelector('#windspeed');
+    windspeed.textContent = jsObject.wind.speed;
+    const precipitation = document.querySelector('#precipitation');
+    //precipitation.textContent = jsObject.
+    const humidity = document.querySelector('#humidity');
+    humidity.textContent = jsObject.main.humidity;
+    console.log(desc[5]); // this shows each letter of what I need.... How to get the whole word?
+    // const windchill = docuemnt.querySelector('#windchill');
+    //where do I find windchill????
+    
+
+
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc.toUPPERCase());
+ });
---
+++ lesson-10/js/weatherapi.js
@@ -0,0 +1,22 @@
+const cityid = "5604473";
+const APPID = "cdc0b1e423f5c1ece1c635b594e5fc76";
+
+
+const apiURL = `http://api.openweathermap.org/data/2.5/weather?id=${cityid}&APPID=${APPID}&units=imperial`;
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    const temperature = document.querySelector('#temperature');
+    temperature.textContent = jsObject.main.temp;
+    const currently = document.querySelector('#currently');
+    const desc = jsObject.weather[0].description;
+    currently.innerHTML = `<strong>${desc.toUPPERCase()}</strong>`;
+
+    const imagesrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc.toUPPERCase());
+ });
