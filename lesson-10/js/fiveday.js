ae44b590aae40166096404f37af24e74fff8c239
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-06-30 23:50:29 -0600

    everything but five day

--- lesson-10/js/fiveday.js
+++ lesson-10/js/fiveday.js
@@ -1,9 +1,18 @@
-const wfURL = 'http://api.openweathermap.org/data/2.5/onecall?lat=42.0963&lon=-111.8766&appid={c982ec73360c04a9960088f0e75fa842}';
+// const lon = '-111.8766';
+// const lat = '42.0963';
+// const appkey = 'c982ec73360c04a9960088f0e75fa842';
 
-fetch(wfURL)
- .then((response) => response.json())
- .then((jsObject) => {
-   console.log(jsObject);
+
+// const wfURL = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}}&lon=${lon}&appid=${appkey}`;
+
+// const wfURL = `http://api.openweathermap.org/data/2.5/?id=${cityid}&APPID=${APPID}&units=imperial`;
+// const wfURL = 'http://api.openweathermap.org/data/2.5/onecall?lat={42.0963}&lon={-111.8766}&appid={c982ec73360c04a9960088f0e75fa842}'; //continues to say its invalid
+
+
+// fetch(wfURL)
+//  .then((response) => response.json())
+//  .then((jsObject) => {
+//    console.log(jsObject);
 //    const days = document.querySelector('#dow');
 //    days.textContent = jsObject.main.temp_max;
 
