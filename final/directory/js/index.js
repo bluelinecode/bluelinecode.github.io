dca3acb72ac5e38048b368c8598f029e2977a1e4
bluelinecode <76972711+bluelinecode@users.noreply.github.com>
2021-07-23 19:18:38 -0600

    updates

--- final/directory/js/index.js
+++ final/directory/js/index.js
@@ -62,3 +62,10 @@ fetch(requestURL)
 
 
     };
+
+const hambutton = document.querySelector('.hamburger');
+const mainnav = document.querySelector('.navigation');
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')}; 
